65a47f8a8843e2ebb0176d170a006e7453a9349f
guatavopalechor <gustavo.palechor@gmail.com>
2017-11-03 11:08:14 +0100

    c++

---
+++ cpp/nombre.cpp
@@ -0,0 +1,7 @@
+#include <iostream> //flujos de datos de entrada y salida. Define in put out put. iostream define el objeto std el cual contiene todos los printf, scan...
+int
+main()
+{
+    std::cout << "The world is a Vampire!" << std::endl;
+    return 0;
+}
