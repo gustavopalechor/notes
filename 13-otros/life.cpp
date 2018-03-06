f6cbea7c7b45501089baea3237daacd2193087d9
guatavopalechor <gustavo.palechor@gmail.com>
2018-03-06 11:31:11 +0100

    life program

--- 13-otros/life.cpp
+++ 13-otros/life.cpp
@@ -1,58 +1,53 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
 
-#define M 0x40
-#define N 0x10
+#include "global.h"
+#include "interfaz.h"
 
-#define CAR_LLENO "▒"
-#define CAR_VACIO " "
+#define MIN_SUPERV 2
+#define ASENTAR    3
 
-#define SON_VALIDAS(f,c) (((f)>=0 && (f)<M) && ((c)>=0 && (c)<N));
-void pintar(int matriz[M][N]){
-    for (int fila=0; fila<N; fila++){
-        for(int col=0; col<N; col++)
-            printf("%s", matriz[fila][col]? CAR_LLENO : CAR_VACIO);
-        printf("\n");
+enum {actual, futuro, TIEMPOS};
 
-    }
-}
-void dame_coord(int *x, int *y){
-  printf("usa unas coordenadas invalidas para terminar.\n");
-  printf("\n\n\tNuevo x,y ");
-  scanf(" %i, %i", x, y);
+int vecinos(int f, int c, int matriz[M][N]){
+    int n_vecinos = 0;
+    for (int fv = f-1; fv < f + 2; fv++)
+        for (int cv=c-1; cv<c+2; cv++)
+            if (SON_VALIDAS(fv, cv))
+                n_vecinos += matriz[fv][cv];
+    n_vecinos--;
+    return n_vecinos;
 }
 
-void poblacion_inicial(int matriz[M][N]){
-      int x, y;
+void calcular(int futuro[M][N], int actual[M][N])
+{
+    int n_vecinos;
 
+    for (int f=0; f<M; f++)
+        for (int c=0; c<N; c++){
+            n_vecinos = vecinos(f, c, actual);
+            if (n_vecinos < MIN_SUPERV)
+                futuro[f][c] = 0;
+            if (n_vecinos ==  ASENTAR)
+                futuro[f][c] = 1;
+            if (n_vecinos >  ASENTAR)
+                futuro[f][c] = 0;
+        }
 
-      do{
+}
+
+int main(){
+
+    int mundo[TIEMPOS][M][N];
+
+    bzero(mundo[actual], sizeof(mundo[actual]));
+    poblacion_inicial(mundo[actual]);
+
+    while(1) {
         system("clear");
+        calcular(mundo[futuro], mundo[actual]);
+        pintar(mundo[actual]);
+    }
 
-        printf("usa unas coordenadas invalidas para terminar.\n");
-        pintar(matriz);
-        printf("\n\tNuevo x,y: ");
-        scanf(" %i, %i", &x, &y);
-        (*x--), (*y--);
-
-        if (SON_VALIDAS(x,y))
-        matriz[x][y] = 1;
-
-        //todo: validar los caracteres antes de meterlos
-
-        }while(es_valido(x,y));
-    int main(){
-
-        int actual[M][N],
-            futuro[M][N];
-      bzero(actual, sizeof(actual));
-      poblacion_inicial(actual);
-    
-      while(1){
-      //calcular(futuro,actual);
-      //convertir el futuro en actual
-      pintar(actual);
-      }
     return EXIT_SUCCESS;
 }
